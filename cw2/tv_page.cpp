30cd0b9a08e36cee720dfe44537037b7e3dcbcf1
Jasmine Tarbard <sc18jt@leeds.ac.uk>
2019-12-09 14:52:49 +0000

    Main code moved to MainWindow class. QStackedWidget implemented to enable window page changes. TvPage base class generated for use in testing. TV Series button socket implemented. Ready for TvPage development.

--- cw2/tv_page.cpp
+++ cw2/tv_page.cpp
@@ -3,11 +3,57 @@
 //
 
 #include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include "vector"
 #include "tv_page.h"
+
+QLabel* cat1_label;
+QLabel* cat2_label;
+QLabel* cat3_label;
 
 QGridLayout* TvPage::layout(QWidget* window, QRect w){
 
     QGridLayout *tvLayout = new QGridLayout();
+    QFont font("Arial", 16);
+
+    cat1_label = new QLabel("Superheroes");
+    cat2_label = new QLabel("Animals");
+    cat3_label = new QLabel("Gameshows");
+
+    tvLayout->addWidget(cat1_label, 0,0,1,2);
+    cat1_label->setFont(font);
+    //Creates Category 1 Labels
+    for ( int i = 0; i < 4; i++ ) {
+        auto* thumbnail = new QPushButton("Placeholder");
+        thumbnail->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+        tvLayout->setColumnMinimumWidth(0,w.width()*0.1);
+        tvLayout->addWidget(thumbnail,1,i,2,1);
+        thumbnail->setFont(font);
+    }
+
+    tvLayout->addWidget(cat2_label, 3,0,1,2);
+    cat2_label->setFont(font);
+    //Creates Category 2 Labels
+    for ( int i = 0; i < 4; i++ ) {
+        auto* thumbnail = new QPushButton("Placeholder");
+        thumbnail->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+        tvLayout->addWidget(thumbnail,4,i,2,1);
+        thumbnail->setFont(font);
+    }
+
+    tvLayout->addWidget(cat3_label, 6,0,1,2);
+    cat3_label->setFont(font);
+    //Creates Category 3 Labels
+    for ( int i = 0; i < 4; i++ ) {
+        auto* thumbnail = new QPushButton("Placeholder");
+        thumbnail->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+        tvLayout->addWidget(thumbnail,7,i,2,1);
+        thumbnail->setFont(font);
+    }
+
+
+
 
     return tvLayout;
 }
